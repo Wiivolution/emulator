f1bbfb3483885f49a54960b849f5a4289b8f7187
Abdelali <abdelalib221@gmail.com>
2026-03-15 00:38:57 +0100

    SHA : Create SHA module.

---
+++ sha.c
@@ -0,0 +1,44 @@
+/*
+    Starmulator - Low Level Wii IOP Emulator
+
+    sha.c - SHA Engine
+    
+    Copyright (C) 2026 Abdelali221
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dev.h"
+
+uint8_t* SHA_Regs;
+
+device SHA = {
+    0, NULL, 0xD02FFFF, 0x1c, 2
+};
+
+int SHA_Init() {
+    SHA_Regs = malloc(0x1c);
+    SHA.ptr = SHA_Regs;
+    printf("\n SHA.ptr: 0x%X", SHA.ptr);
+    Dev_AddDevice(&SHA);
+    return 0;
+}
+
+int SHA_Deinit() {
+
+}
